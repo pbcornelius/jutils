45b97b90b122a18c2c94c8b996d03b8c4c51ef5c
pbcornelius <cornelius@rsm.nl>
2024-11-22 16:03:55 +0100

    11
    - CSVReader: r/m BOM; readAllMaps() impl.

--- src/de/pbc/utils/csv/CSVReader.java
+++ src/de/pbc/utils/csv/CSVReader.java
@@ -1,38 +1,72 @@
 package de.pbc.utils.csv;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.commons.io.input.BOMInputStream;
+
+import com.opencsv.exceptions.CsvRuntimeException;
 
 public class CSVReader extends com.opencsv.CSVReader {
-	
-	// CONSTANTS ----------------------------------------------------- //
-	
-	public static String DEFAULT_CHARSET = "UTF-8";
-	
+
 	// CONSTRUCTOR --------------------------------------------------- //
-	
-	public CSVReader(String path) throws IOException {
-		super(Files.newBufferedReader(Paths.get(path), Charset.forName(DEFAULT_CHARSET)));
-	}
-	
-	public CSVReader(String path, String charset) throws IOException {
-		super(Files.newBufferedReader(Paths.get(path), Charset.forName(charset)));
-	}
-	
-	public CSVReader(Path path) throws IOException {
-		super(Files.newBufferedReader(path, Charset.forName(DEFAULT_CHARSET)));
-	}
-	
-	public CSVReader(Path path, String charset) throws IOException {
-		super(Files.newBufferedReader(path, Charset.forName(charset)));
-	}
-	
+
 	public CSVReader(Reader reader) {
 		super(reader);
 	}
-	
+
+	/**
+	 * Removes byte-order-mark (BOM) typically added by Excel.
+	 */
+	public CSVReader(String path) throws IOException {
+		this(Path.of(path));
+	}
+
+	/**
+	 * Removes byte-order-mark (BOM) typically added by Excel.
+	 */
+	public CSVReader(Path path) throws IOException {
+		super(new BufferedReader(new InputStreamReader(new BOMInputStream(Files.newInputStream(path)), "UTF-8")));
+	}
+
+	// PUBLIC -------------------------------------------------------- //
+
+	/**
+	 * Reads CSV into list of maps. The first line contains the map keys.
+	 * 
+	 * @throws CsvRuntimeException no lines must have been read before
+	 */
+	public List<Map<String, String>> readAllMaps() throws IOException, CsvRuntimeException {
+		if (getRecordsRead() > 0) {
+			throw new CsvRuntimeException("no lines must have been read");
+		}
+
+		List<Map<String, String>> output = new LinkedList<>();
+		String[] headers = readNext();
+		Objects.requireNonNull(headers);
+		for (String header : headers) {
+			Objects.requireNonNull(header);
+		}
+
+		String[] line = readNext();
+		while (line != null) {
+			Map<String, String> map = new HashMap<>(line.length);
+			for (int i = 0; i < line.length; i++) {
+				map.put(headers[i], line[i]);
+			}
+			output.add(map);
+			line = readNext();
+		}
+		return output;
+	}
+
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,13 +1,15 @@
 module de.pbc.utils {
-	
+
 	exports de.pbc.utils;
-	
+
 	exports de.pbc.utils.csv;
-	
+
 	requires java.xml;
-	
+
 	requires static opencsv;
-	
+
 	requires static org.apache.commons.lang3;
-	
+
+	requires static org.apache.commons.io;
+
 }
